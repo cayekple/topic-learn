4601a4ae3742b8643fda0e2920d4706055dc59df
Clemence Ayekple <clemence.ayekple@ucc.edu.gh>
2020-10-30 23:41:12 +0000

    getting started with the homepage

--- app/src/main/java/com/cayekple/topiclearn/MainActivity.java
+++ app/src/main/java/com/cayekple/topiclearn/MainActivity.java
@@ -1,14 +1,74 @@
 package com.cayekple.topiclearn;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mainToolbar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mainToolbar = findViewById(R.id.mainToolbar);
+        setSupportActionBar(mainToolbar);
+        getSupportActionBar().setTitle("Topic Learn");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null){
+            sendToLogin();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.actionLogoutBtn:
+                logOut();
+                return true;
+            case R.id.actionSettingBtn:
+                Intent settingIntent =  new Intent(MainActivity.this, AccountSetting.class);
+                startActivity(settingIntent);
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private void logOut() {
+        mAuth.signOut();
+        sendToLogin();
+    }
+
+    private void sendToLogin() {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/cayekple/topiclearn/SplashActivity.java
+++ app/src/main/java/com/cayekple/topiclearn/SplashActivity.java
@@ -2,7 +2,10 @@ package com.cayekple.topiclearn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Button btn = findViewById(R.id.btnGettingStarted);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
