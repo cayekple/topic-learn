373b86e42bb94070771b4f3d8a1c9d5fdfa282d8
Clemence Ayekple <clemence.ayekple@ucc.edu.gh>
2020-10-30 23:36:11 +0000

    login, register and accounting settings added

---
+++ app/src/main/java/com/cayekple/topiclearn/AccountSetting.java
@@ -0,0 +1,14 @@
+package com.cayekple.topiclearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_setting);
+    }
+}
